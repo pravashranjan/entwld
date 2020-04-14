97cc3dff7fa93281d947db56d7956c8f78d1219b
pravashranjan <pravashranjan@gmail.com>
2020-04-14 09:17:18 +0530

    Add files via upload

---
+++ ENT2/js/nav.js
@@ -0,0 +1,67 @@
+/**
+ * The nav stuff
+ */
+(function( window ){
+	
+	'use strict';
+
+	var body = document.body,
+		mask = document.createElement("div"),
+		toggleSlideLeft = document.querySelector( ".toggle-slide-left" ),
+		toggleSlideRight = document.querySelector( ".toggle-slide-right" ),
+		toggleSlideTop = document.querySelector( ".toggle-slide-top" ),
+		toggleSlideBottom = document.querySelector( ".toggle-slide-bottom" ),
+		slideMenuLeft = document.querySelector( ".slide-menu-left" ),
+		slideMenuRight = document.querySelector( ".slide-menu-right" ),
+		slideMenuTop = document.querySelector( ".slide-menu-top" ),
+		slideMenuBottom = document.querySelector( ".slide-menu-bottom" ),
+		activeNav
+	;
+	mask.className = "mask";
+
+	/* slide menu left */
+	toggleSlideLeft.addEventListener( "click", function(){
+		classie.add( body, "sml-open" );
+		document.body.appendChild(mask);
+		activeNav = "sml-open";
+	} );
+
+	/* slide menu right */
+	toggleSlideRight.addEventListener( "click", function(){
+		classie.add( body, "smr-open" );
+		document.body.appendChild(mask);
+		activeNav = "smr-open";
+	} );
+
+	/* slide menu top */
+	toggleSlideTop.addEventListener( "click", function(){
+		classie.add( body, "smt-open" );
+		document.body.appendChild(mask);
+		activeNav = "smt-open";
+	} );
+
+	/* slide menu bottom */
+	toggleSlideBottom.addEventListener( "click", function(){
+		classie.add( body, "smb-open" );
+		document.body.appendChild(mask);
+		activeNav = "smb-open";
+	} );
+
+	/* hide active menu if mask is clicked */
+	mask.addEventListener( "click", function(){
+		classie.remove( body, activeNav );
+		activeNav = "";
+		document.body.removeChild(mask);
+	} );
+
+	/* hide active menu if close menu button is clicked */
+	[].slice.call(document.querySelectorAll(".close-menu")).forEach(function(el,i){
+		el.addEventListener( "click", function(){
+			classie.remove( body, activeNav );
+			activeNav = "";
+			document.body.removeChild(mask);
+		} );
+	});
+
+
+})( window );
